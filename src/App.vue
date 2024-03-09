b6fccae3b77111c38bce790678fe9a20c5ed3203
Radoslaw Przewuski <radekprzewuski@gmail.com>
2024-03-09 20:28:21 +0100

    initial site commit

---
+++ src/App.vue
@@ -0,0 +1,197 @@
+<template>
+  <div id="body" :class="{ dark: darkTheme }">
+    <nav
+      class="z-10 antialiased bg-neutral-200 dark:bg-neutral-800 shadow-md dark:shadow-none border-gray-200 sticky top-0"
+    >
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
+        <RouterLink to="/">
+          <div class="flex items-center space-x-3 rtl:space-x-reverse">
+            <template v-if="isMac">
+              <img
+                v-if="darkTheme"
+                class="h-7 m-1"
+                src="@/assets/logo_dark.svg"
+                alt="Bridging the Gap logo"
+              />
+              <img v-else class="h-7 m-1" src="@/assets/logo.svg" alt="Bridging the Gap logo" />
+            </template>
+            <template v-else>
+              <img class="h-7 m-1" src="@/assets/logo.svg" alt="Bridging the Gap logo" />
+            </template>
+            <div
+              class="text-base mt-1 font-semibold whitespace-nowrap hidden xs:block text-black dark:text-gray-50 tracking-wide"
+            >
+              Bridging the Gap
+            </div>
+          </div>
+        </RouterLink>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div class="antialiased hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul
+            class="flex items-center flex-col gap-1 md:gap-0 p-4 md:p-0 mt-4 border bg-transparent border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0"
+          >
+            <li>
+              <RouterLink
+                to="/about"
+                class="text-black dark:text-white navigation-button"
+                aria-current="page"
+                >About us
+              </RouterLink>
+            </li>
+            <li class="flex items-center">
+              <button aria-label="Change theme" class="mr-1" type="button" @click="changeTheme">
+                <SvgIcon
+                  class="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white card"
+                  type="mdi"
+                  :path="mdiThemeLightDark"
+                  :size="32"
+                />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <main
+      class="antialiased bg-neutral-100 dark:bg-neutral-950 text-black dark:text-gray-50 w-full px-4 py-4 flex justify-center items-stretch flex-grow"
+    >
+      <RouterView v-slot="{ Component }" class="max-w-screen-xl flex flex-grow">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </RouterView>
+    </main>
+    <footer
+      class="sticky bottom-0 w-full shadow-up dark:shadow-none antialiased pr-4 py-1 hidden md:flex bg-neutral-200 dark:bg-neutral-800 text-black dark:text-white"
+    >
+      <div class="flex mx-2 justify-start max-w-screen-xl w-full text-[0.65rem]">
+        <a
+          href="https://github.com/skrzymek/BridgingTheGap"
+          property="dct:title"
+          rel="cc:attributionURL"
+          target="_blank"
+          ><b>Bridging the Gap</b></a
+        >
+        &nbsp;© 2024 by&nbsp;
+        <a
+          href="https://www.linkedin.com/in/karolskrzymowski/"
+          property="cc:attributionName"
+          rel="cc:attributionURL dct:creator"
+          target="_blank"
+          class="link text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+          ><b>Karol Skrzymowski</b></a
+        >
+        <div>&nbsp;and&nbsp;</div>
+        <a
+          href="https://www.linkedin.com/in/rados%C5%82aw-przewuski/"
+          property="cc:attributionName"
+          rel="cc:attributionURL dct:creator"
+          target="_blank"
+          class="link text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+          ><b>Radosław Przewuski</b></a
+        >
+        &nbsp;is licensed under
+        <a href="https://creativecommons.org/licenses/by/4.0" target="_blank" class="flex">
+          &nbsp;CC BY 4.0
+          <img
+            alt=""
+            class="cc_image mx-1 self-center"
+            src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
+          />
+          <img
+            alt=""
+            class="cc_image"
+            src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+          />
+        </a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { initFlowbite } from 'flowbite'
+import { onMounted, ref } from 'vue'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiThemeLightDark } from '@mdi/js'
+
+const darkTheme = ref(false)
+const isMac = ref(false)
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite()
+
+  isMac.value = navigator.userAgent.includes('Mac OS')
+  document.querySelector(':root').style.setProperty('--macDarkVisible', 'none')
+
+  if (localStorage.theme) {
+    darkTheme.value = localStorage.theme === 'true'
+  } else {
+    darkTheme.value = window.matchMedia('(prefers-color-scheme: dark)').matches
+  }
+
+  let metaThemeColor = document.querySelector('meta[name=theme-color]')
+  if (darkTheme.value === true) {
+    metaThemeColor.setAttribute('content', 'rgb(10,10,10)')
+  } else {
+    metaThemeColor.setAttribute('content', 'rgb(245,245,245)')
+  }
+
+  if (isMac.value) {
+    document
+      .querySelector(':root')
+      .style.setProperty('--macDarkVisible', darkTheme.value ? 'initial' : 'none')
+    document
+      .querySelector(':root')
+      .style.setProperty('--macDarkInvisible', darkTheme.value ? 'none' : 'initial')
+  }
+})
+
+const changeTheme = () => {
+  darkTheme.value = !darkTheme.value
+  localStorage.theme = darkTheme.value
+
+  let metaThemeColor = document.querySelector('meta[name=theme-color]')
+  if (darkTheme.value === true) {
+    metaThemeColor.setAttribute('content', 'rgb(10,10,10)')
+  } else {
+    metaThemeColor.setAttribute('content', 'rgb(245,245,245)')
+  }
+
+  if (isMac.value) {
+    document
+      .querySelector(':root')
+      .style.setProperty('--macDarkVisible', darkTheme.value ? 'initial' : 'none')
+    document
+      .querySelector(':root')
+      .style.setProperty('--macDarkInvisible', darkTheme.value ? 'none' : 'initial')
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/ArticlePage.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="flex justify-between w-full articleLinkHeight">
+    <div class="flex justify-center flex-grow">
+      <div class="article">
+        <div class="mb-5">
+          <router-link to="/"
+                       class="flex link items-end text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white">
+            <SvgIcon
+              type="mdi"
+              :path="mdiArrowLeft"
+              :size="24"
+            />
+            <h5 class="my-0 ml-1">Back to articles list</h5>
+          </router-link>
+        </div>
+        <h1 class="mt-3 mb-2.5">{{ title }}</h1>
+        <div class="text-justify hyphens-auto my-2 block lg:hidden">
+          <router-link
+            class="pr-2 navigation-button text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-gray-50"
+            v-for="tag of tags"
+            :key="tag"
+            :to="tagLink(tag)"
+          >
+            #{{ tag }}
+          </router-link>
+        </div>
+        <div class="my-2 border-b dark:border-neutral-800 lg:hidden" />
+        <div class="text-justify hyphens-auto mb-2 block lg:hidden">
+          <div class="text-gray-600 dark:text-gray-400">Published</div>
+          <div><b>{{ published }}</b></div>
+          <div class="text-gray-600 dark:text-gray-400">Last update</div>
+          <div><b>{{ lastUpdate }}</b></div>
+        </div>
+        <div class="lg:hidden">
+          <div class="mt-3 border-b dark:border-neutral-800" />
+          <h4>Authors</h4>
+          <div v-for="author of authors" :key="author.author">
+            <a target="_blank" class="link text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-gray-50"
+               :href="author.link">{{ author.author }}</a>
+          </div>
+        </div>
+        <div class="my-2 border-b dark:border-neutral-800 lg:hidden" />
+        <div v-html="articleContent" />
+        <div class="lg:hidden" v-if="reviewers.length > 0">
+          <div class="mt-3 border-b dark:border-neutral-800" />
+          <h4>Article Reviewers</h4>
+          <div v-for="reviewer of sortedReviewers" :key="reviewer.reviewer">
+            <a target="_blank" class="link text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-gray-50"
+               :href="reviewer.link">{{ reviewer.reviewer }}</a>
+          </div>
+        </div>
+        <div v-if="bibliography.length > 0">
+          <div class="mt-3 border-b dark:border-neutral-800" />
+          <h4>Bibliography</h4>
+          <div class="articleLinkHeight" v-for="bib of bibliography" :key="bib.title">
+            <a class="link" target="_blank"
+               :href="bib.link">{{
+                bib.title
+              }}</a>
+            <span>;&nbsp;{{ bib.author }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-[10rem] self-stretch border-l dark:border-neutral-900 ml-3 text-right hidden lg:block">
+      <div class="mx-3 sticky top-24">
+        <h4 class="mt-0 mb-2">Tags</h4>
+        <div>
+          <router-link
+            class="inline-block pl-2 pt-1 text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-gray-50"
+            v-for="tag of tags"
+            :key="tag"
+            :to="tagLink(tag)"
+          >#{{ tag }}
+          </router-link>
+        </div>
+        <div class="mt-3 border-b dark:border-neutral-800" v-if="reviewers.length > 0" />
+        <div class="text-right hyphens-auto mb-2 mt-3">
+          <h4 class="mt-0 mb-2">Authors</h4>
+          <div v-for="author of authors" :key="author.author">
+            <a target="_blank" class="link text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-gray-50"
+               :href="author.link">{{ author.author }}</a>
+          </div>
+        </div>
+        <div class="mt-3 border-b dark:border-neutral-800" />
+        <div class="text-right hyphens-auto mb-2 mt-3" v-if="reviewers.length > 0">
+          <h4 class="mt-0 mb-2">Article Reviewers</h4>
+          <div v-for="reviewer of sortedReviewers" :key="reviewer.reviewer">
+            <a target="_blank" class="link text-gray-600 dark:text-gray-400 hover:text-black hover:dark:text-gray-50"
+               :href="reviewer.link">{{ reviewer.reviewer }}</a>
+          </div>
+        </div>
+        <div class="mt-3 border-b dark:border-neutral-800" />
+        <div class="text-right hyphens-auto mb-2 mt-3">
+          <div class="text-gray-600 dark:text-gray-400">Published</div>
+          <div><b>{{ published }}</b></div>
+          <div class="text-gray-600 dark:text-gray-400">Last update</div>
+          <div><b>{{ lastUpdate }}</b></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, onBeforeMount, ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import articles from '../../articles.json' with { type: 'json' }
+import axios from 'axios'
+import { mdiArrowLeft } from '@mdi/js'
+import SvgIcon from '@jamescoyle/vue-icon'
+
+const route = useRoute()
+const router = useRouter()
+
+const articleName = ref('')
+const articleContent = ref('')
+const title = ref('')
+const published = ref('')
+const lastUpdate = ref('')
+const bibliography = ref([])
+const reviewers = ref([])
+const authors = ref([])
+
+const tags = ref([])
+
+onBeforeMount(async () => {
+  await updateData()
+})
+
+watch(route, async () => {
+  await updateData()
+})
+
+const sortedReviewers = computed(() => {
+  if (reviewers.value) {
+    return reviewers.value.sort((a, b) => {
+      return a.reviewer.localeCompare(b.reviewer)
+    })
+  }
+
+  return []
+})
+
+const updateData = async () => {
+  articleName.value = route.params.article
+
+  bibliography.value = []
+
+  const articleMeta = articles.find(el => {
+    return el.name === articleName.value
+  })
+
+  if (articleMeta) {
+    title.value = articleMeta.title
+    published.value = new Date(articleMeta.publishDate).toLocaleDateString()
+    lastUpdate.value = new Date(articleMeta.lastUpdate).toLocaleDateString()
+    tags.value = articleMeta.tags ? articleMeta.tags : []
+    bibliography.value = articleMeta.bibliography ? articleMeta.bibliography : []
+    reviewers.value = articleMeta.reviewers ? articleMeta.reviewers : []
+    authors.value = articleMeta.authors ? articleMeta.authors : []
+  }
+
+  await getArticleContent()
+}
+
+const tagLink = (tag) => {
+  if (route.query && route.query.tags) {
+    if (!route.query.tags.split(',').includes(tag)) {
+      return `/?tags=${route.query.tags + ',' + tag}`
+    } else {
+      const filter = route.query.tags.split(',').filter((el) => {
+        return el !== tag
+      })
+      if (filter.length === 0) {
+        return `/`
+      } else {
+        return `/?tags=${filter}`
+      }
+    }
+  } else {
+    return `/?tags=${tag}`
+  }
+}
+
+const getArticleContent = async () => {
+  try {
+    const response = await axios.get(`/bridgingTheGap/articles/${articleName.value}/html/index.html`)
+
+    if (response.status === 200) {
+      articleContent.value = response.data
+    }
+  } catch (e) {
+    // IGNORE ??
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
