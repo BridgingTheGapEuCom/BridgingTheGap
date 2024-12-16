6d27f07ccf36bf74d6dd418fea3a3e9b507732d7
Radoslaw Przewuski <radekprzewuski@gmail.com>
2024-12-16 17:06:00 +0100

    new article added

--- error.vue
+++ error.vue
@@ -1,18 +1,12 @@
 <template>
-  <div
-    class="w-full h-screen text-black flex flex-col items-center justify-center"
-  >
+  <div class="w-full h-screen text-black flex flex-col items-center justify-center">
     <div class="big">Are you lost?</div>
-    <button @click="handleError" class="link">
-      Go back to the articles page
-    </button>
+    <button class="link" @click="handleError">Go back to the articles page</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { NuxtError } from "#app";
-
-const handleError = () => clearError({ redirect: "/" });
+const handleError = () => clearError({ redirect: '/' })
 </script>
 
 <style lang="scss" scoped>
