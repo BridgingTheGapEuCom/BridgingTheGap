e7d9f767bcb3ea0d223656a9091635748422ad7d
Radoslaw Przewuski <radekprzewuski@gmail.com>
2025-03-23 13:35:31 +0100

    cookie expiration fix

--- plugins/appStart.ts
+++ plugins/appStart.ts
@@ -3,7 +3,7 @@ export default defineNuxtPlugin({
   enforce: 'pre',
   hooks: {
     'app:created'() {
-      const cookie = useCookie('theme')
+      const cookie = useCookie('theme', { maxAge: 31556926 })
       if (cookie.value !== undefined) {
         useState('dark', () => cookie.value)
       } else {
