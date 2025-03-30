c6a554715dd428fce82ab8f6d3841a9e42ff660d
Radoslaw Przewuski <radekprzewuski@gmail.com>
2025-03-30 18:47:30 +0200

    new article

---
+++ components/articles/Event-Driven_Polling/Event-Driven_Polling-key-based-contract_details.vue
@@ -0,0 +1,443 @@
+<template>
+  <div
+    tabindex="0"
+    aria-label="Diagram showing two arrows originating from a cube labeled 'Event producing system' The top arrow curves right to a cube labeled 'Event consuming system', and another arrow curves down and then right to a second cube labeled 'Event consuming system'. Additionally, curved arrows point back from each 'Event consuming system' cube to the 'Event producing system' cube."
+    role="img"
+    class="syncSvgParent w-full"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style="color-scheme: light dark"
+      width="500px"
+      height="276px"
+      viewBox="-0.5 -0.5 500 276"
+    >
+      <g>
+        <g>
+          <g>
+            <g>
+              <g>
+                <g />
+              </g>
+              <g>
+                <g>
+                  <g>
+                    <svg
+                      viewBox="0 0 850 850"
+                      x="59.5"
+                      y="79"
+                      width="77"
+                      height="77"
+                    >
+                      <g>
+                        <path
+                          d="M 230 280 l 157.18 72.352 c 20.668 9.5156 54.496 9.5156 75.145 0 l 132.64 -61.055 l 217.53 -103.74 c -4.5742 -3.4336 -9.25 -6.5156 -14.191 -8.7734 l -317.09 -146.01 c -30.992 -14.277 -81.746 -14.277 -112.76 0 l -317.19 146.01 c -5.0703 2.3164 -9.8672 5.5234 -14.543 9.0625 z"
+                          class="top"
+                        />
+                        <path
+                          d="m 423.34 420.96 c 0 -22.801 -16.902 -49.16 -37.574 -58.695 l -239.42 -110.15 c -0.207 -0.082 -0.3711 -0.25 -0.5781 -0.3516 l -108.25 -49.883 v 372.25 c 0 34.137 25.387 73.738 56.398 88.035 l 317.19 146.03 c 2.8125 1.3047 5.957 2.3398 9.0625 3.3945 l 3.1445 -193.8 l 0.0039 -196.82 z"
+                          class="left"
+                        />
+                        <path
+                          d="m 708.59 250.96 c -0.4336 0.2266 -0.8086 0.5391 -1.2812 0.7656 l -239.46 110.21 c -20.691 9.4961 -37.574 35.918 -37.574 58.676 v 162.12 l 0.4766 -0.0195 l -0.7461 229.8 c 4.4688 -1.3438 8.7109 -2.9375 12.621 -4.7383 l 317.17 -146.03 c 30.992 -14.277 56.398 -53.875 56.398 -88.035 l -0.0625 -372.39 z"
+                          class="right"
+                        />
+                      </g>
+                    </svg>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g>
+              <g>
+                <g />
+              </g>
+              <g>
+                <g>
+                  <g>
+                    <svg
+                      viewBox="0 0 850 850"
+                      x="419.5"
+                      y="148.96"
+                      width="77"
+                      height="77"
+                    >
+                      <g>
+                        <path
+                          d="M 230 280 l 157.18 72.352 c 20.668 9.5156 54.496 9.5156 75.145 0 l 132.64 -61.055 l 217.53 -103.74 c -4.5742 -3.4336 -9.25 -6.5156 -14.191 -8.7734 l -317.09 -146.01 c -30.992 -14.277 -81.746 -14.277 -112.76 0 l -317.19 146.01 c -5.0703 2.3164 -9.8672 5.5234 -14.543 9.0625 z"
+                          class="top"
+                        />
+                        <path
+                          d="m 423.34 420.96 c 0 -22.801 -16.902 -49.16 -37.574 -58.695 l -239.42 -110.15 c -0.207 -0.082 -0.3711 -0.25 -0.5781 -0.3516 l -108.25 -49.883 v 372.25 c 0 34.137 25.387 73.738 56.398 88.035 l 317.19 146.03 c 2.8125 1.3047 5.957 2.3398 9.0625 3.3945 l 3.1445 -193.8 l 0.0039 -196.82 z"
+                          class="left"
+                        />
+                        <path
+                          d="m 708.59 250.96 c -0.4336 0.2266 -0.8086 0.5391 -1.2812 0.7656 l -239.46 110.21 c -20.691 9.4961 -37.574 35.918 -37.574 58.676 v 162.12 l 0.4766 -0.0195 l -0.7461 229.8 c 4.4688 -1.3438 8.7109 -2.9375 12.621 -4.7383 l 317.17 -146.03 c 30.992 -14.277 56.398 -53.875 56.398 -88.035 l -0.0625 -372.39 z"
+                          class="right"
+                        />
+                      </g>
+                    </svg>
+                  </g>
+                </g>
+              </g>
+              <g>
+                <g>
+                  <g>
+                    <rect
+                      x="428.83"
+                      y="227.39"
+                      width="62.34"
+                      height="47.79"
+                      fill="none"
+                      stroke="none"
+                      pointer-events="all"
+                    />
+                  </g>
+                  <g>
+                    <g transform="translate(-0.5 -0.5)">
+                      <switch>
+                        <foreignObject
+                          style="overflow: visible; text-align: left"
+                          pointer-events="none"
+                          width="100%"
+                          height="100%"
+                          requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
+                        >
+                          <div
+                            xmlns="http://www.w3.org/1999/xhtml"
+                            style="
+                              display: flex;
+                              align-items: unsafe center;
+                              justify-content: unsafe center;
+                              width: 60px;
+                              height: 1px;
+                              padding-top: 251px;
+                              margin-left: 430px;
+                            "
+                          >
+                            <div style="box-sizing: border-box; font-size: 0; text-align: center">
+                              <div
+                                style="
+                                  display: inline-block;
+                                  font-size: 12px;
+                                  line-height: 1.2;
+                                  pointer-events: all;
+                                  white-space: normal;
+                                  word-wrap: normal;
+                                "
+                              >
+                                Event<br>consuming<br>system
+                              </div>
+                            </div>
+                          </div>
+                        </foreignObject>
+                        <text
+                          x="460"
+                          y="255"
+                          fill="light-dark(#000000, #ffffff)"
+                          font-size="12px"
+                          text-anchor="middle"
+                        >
+                          Event cons...
+                        </text>
+                      </switch>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g>
+              <g>
+                <g />
+              </g>
+              <g>
+                <g>
+                  <g>
+                    <svg
+                      viewBox="0 0 850 850"
+                      x="419.5"
+                      y="-1"
+                      width="77"
+                      height="77"
+                    >
+                      <g>
+                        <path
+                          d="M 230 280 l 157.18 72.352 c 20.668 9.5156 54.496 9.5156 75.145 0 l 132.64 -61.055 l 217.53 -103.74 c -4.5742 -3.4336 -9.25 -6.5156 -14.191 -8.7734 l -317.09 -146.01 c -30.992 -14.277 -81.746 -14.277 -112.76 0 l -317.19 146.01 c -5.0703 2.3164 -9.8672 5.5234 -14.543 9.0625 z"
+                          class="top"
+                        />
+                        <path
+                          d="m 423.34 420.96 c 0 -22.801 -16.902 -49.16 -37.574 -58.695 l -239.42 -110.15 c -0.207 -0.082 -0.3711 -0.25 -0.5781 -0.3516 l -108.25 -49.883 v 372.25 c 0 34.137 25.387 73.738 56.398 88.035 l 317.19 146.03 c 2.8125 1.3047 5.957 2.3398 9.0625 3.3945 l 3.1445 -193.8 l 0.0039 -196.82 z"
+                          class="left"
+                        />
+                        <path
+                          d="m 708.59 250.96 c -0.4336 0.2266 -0.8086 0.5391 -1.2812 0.7656 l -239.46 110.21 c -20.691 9.4961 -37.574 35.918 -37.574 58.676 v 162.12 l 0.4766 -0.0195 l -0.7461 229.8 c 4.4688 -1.3438 8.7109 -2.9375 12.621 -4.7383 l 317.17 -146.03 c 30.992 -14.277 56.398 -53.875 56.398 -88.035 l -0.0625 -372.39 z"
+                          class="right"
+                        />
+                      </g>
+                    </svg>
+                  </g>
+                </g>
+              </g>
+              <g>
+                <g>
+                  <g>
+                    <rect
+                      x="428.83"
+                      y="76.39"
+                      width="62.34"
+                      height="47.79"
+                      fill="none"
+                      stroke="none"
+                      pointer-events="all"
+                    />
+                  </g>
+                  <g>
+                    <g transform="translate(-0.5 -0.5)">
+                      <switch>
+                        <foreignObject
+                          style="overflow: visible; text-align: left"
+                          pointer-events="none"
+                          width="100%"
+                          height="100%"
+                          requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
+                        >
+                          <div
+                            xmlns="http://www.w3.org/1999/xhtml"
+                            style="
+                              display: flex;
+                              align-items: unsafe center;
+                              justify-content: unsafe center;
+                              width: 60px;
+                              height: 1px;
+                              padding-top: 100px;
+                              margin-left: 430px;
+                            "
+                          >
+                            <div style="box-sizing: border-box; font-size: 0; text-align: center">
+                              <div
+                                style="
+                                  display: inline-block;
+                                  font-size: 12px;
+                                  line-height: 1.2;
+                                  pointer-events: all;
+                                  white-space: normal;
+                                  word-wrap: normal;
+                                "
+                              >
+                                Event<br>consuming<br>system
+                              </div>
+                            </div>
+                          </div>
+                        </foreignObject>
+                        <text
+                          x="460"
+                          y="104"
+                          fill="light-dark(#000000, #ffffff)"
+                          font-size="12px"
+                          text-anchor="middle"
+                        >
+                          Event...
+                        </text>
+                      </switch>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g>
+              <g>
+                <g>
+                  <rect
+                    x="0"
+                    y="99"
+                    width="60"
+                    height="46"
+                    fill="none"
+                    stroke="none"
+                    pointer-events="all"
+                  />
+                </g>
+                <g>
+                  <g transform="translate(-0.5 -0.5)">
+                    <switch>
+                      <foreignObject
+                        style="overflow: visible; text-align: left"
+                        pointer-events="none"
+                        width="100%"
+                        height="100%"
+                        requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
+                      >
+                        <div
+                          xmlns="http://www.w3.org/1999/xhtml"
+                          style="
+                            display: flex;
+                            align-items: unsafe center;
+                            justify-content: unsafe center;
+                            width: 58px;
+                            height: 1px;
+                            padding-top: 122px;
+                            margin-left: 1px;
+                          "
+                        >
+                          <div style="box-sizing: border-box; font-size: 0; text-align: center">
+                            <div
+                              style="
+                                display: inline-block;
+                                font-size: 12px;
+                                line-height: 1.2;
+                                pointer-events: all;
+                                white-space: normal;
+                                word-wrap: normal;
+                              "
+                            >
+                              Event<br>producing<br>system
+                            </div>
+                          </div>
+                        </div>
+                      </foreignObject>
+                      <text
+                        x="30"
+                        y="126"
+                        fill="light-dark(#000000, #ffffff)"
+                        font-size="12px"
+                        text-anchor="middle"
+                      >
+                        Event prod...
+                      </text>
+                    </switch>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+          <g>
+            <g>
+              <g class="arrow">
+                <g>
+                  <path
+                    d="M 420 39.5 L 130 39.02 Q 120 39 120 49 L 120 73.13"
+                    stroke-miterlimit="10"
+                    pointer-events="stroke"
+                    class="arrow"
+                  />
+                  <path
+                    d="M 120 78.38 L 116.5 71.38 L 120 73.13 L 123.5 71.38 Z"
+                    stroke-miterlimit="10"
+                    pointer-events="all"
+                    class="arrow"
+                  />
+                </g>
+              </g>
+            </g>
+            <g>
+              <g class="arrow">
+                <g>
+                  <path
+                    d="M 420 189.46 L 130 189.02 Q 120 189 120 179 L 120 165.87"
+                    stroke-miterlimit="10"
+                    pointer-events="stroke"
+                    class="arrow"
+                  />
+                  <path
+                    d="M 120 160.62 L 123.5 167.62 L 120 165.87 L 116.5 167.62 Z"
+                    stroke-miterlimit="10"
+                    pointer-events="all"
+                    class="arrow"
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+          <g>
+            <g>
+              <g class="arrow">
+                <g>
+                  <path
+                    d="M 80 79.5 L 80 30 Q 80 20 90 19.99 L 413.63 19.51"
+                    stroke-miterlimit="10"
+                    pointer-events="stroke"
+                    class="arrow"
+                  />
+                  <path
+                    d="M 418.88 19.5 L 411.89 23.01 L 413.63 19.51 L 411.88 16.01 Z"
+                    stroke-miterlimit="10"
+                    pointer-events="all"
+                    class="arrow"
+                  />
+                </g>
+              </g>
+            </g>
+            <g>
+              <g class="arrow">
+                <g>
+                  <path
+                    d="M 80 159.5 L 80 197 Q 80 207 90 207.07 L 413.63 209.41"
+                    stroke-miterlimit="10"
+                    pointer-events="stroke"
+                    class="arrow"
+                  />
+                  <path
+                    d="M 418.88 209.45 L 411.86 212.9 L 413.63 209.41 L 411.91 205.9 Z"
+                    stroke-miterlimit="10"
+                    pointer-events="all"
+                    class="arrow"
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.syncSvgParent {
+  transition: var(--diagramColorTransition);
+}
+
+.top {
+  fill: var(--tw-color-gray-500);
+  stroke: black;
+  stroke-width: 2;
+
+  .dark > * & {
+    fill: var(--tw-color-gray-50);
+  }
+}
+.left {
+  fill: var(--tw-color-gray-600);
+  stroke: black;
+  stroke-width: 2;
+
+  .dark > * & {
+    fill: var(--tw-color-gray-200);
+  }
+}
+.right {
+  fill: var(--tw-color-gray-800);
+  stroke: black;
+  stroke-width: 2;
+
+  .dark > * & {
+    fill: var(--tw-color-gray-400);
+  }
+}
+.arrow {
+  stroke: var(--tw-color-black);
+  fill: none;
+
+  .dark > * & {
+    stroke: var(--tw-color-white);
+  }
+}
+.arrow:nth-child(2) {
+  fill: var(--tw-color-black);
+
+  .dark > * & {
+    fill: var(--tw-color-white);
+  }
+}
+</style>
