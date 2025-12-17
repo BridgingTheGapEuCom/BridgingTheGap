25bf6d40162698d8c4379bfd4c8e7331bd7c8628
Radoslaw Przewuski <radekprzewuski@gmail.com>
2025-12-17 21:37:26 +0100

    new streams

---
+++ pages/streams/Loosely_Coupled_-_Interoperability_in_Micro-frontends/video.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full">
+    <StreamLayout
+      :transcript="transcript"
+      name="Loosely Coupled - Interoperability in Micro-frontends"
+      video-id="PBW9Ls-VCoY"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const transcript = ref()
+</script>
+
+<style scoped></style>
---
+++ pages/streams/Loosely_Coupled_-_Organizing_a_conference;_Behind_the_scenes/video.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full">
+    <StreamLayout
+      :transcript="transcript"
+      name="Loosely Coupled - Organizing a conference Behind the scenes"
+      video-id="c6U63Z1w3Yc"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const transcript = ref()
+</script>
+
+<style scoped></style>
