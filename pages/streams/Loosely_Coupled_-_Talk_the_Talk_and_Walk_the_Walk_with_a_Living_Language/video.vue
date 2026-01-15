e25888fc3e3fe6fd6e6c1ec1c05397e053ea4627
Radoslaw Przewuski <radekprzewuski@gmail.com>
2026-01-15 14:47:18 +0100

    stream page update and packages update

---
+++ pages/streams/Loosely_Coupled_-_Talk_the_Talk_and_Walk_the_Walk_with_a_Living_Language/video.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full">
+    <StreamLayout
+      :transcript="transcript"
+      name="Loosely Coupled - Talk the Talk and Walk the Walk with a Living Language"
+      video-id="cjxESvptq10"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const transcript = ref()
+</script>
+
+<style scoped></style>
